c57c3eab93b79566e9826bde10ae7b3984810db9
Micha Davis <m.leigh.davis@gmail.com>
2022-03-28 21:43:43 -0700

    fix typo in comments

--- auth/routes/signup.js
+++ auth/routes/signup.js
@@ -8,7 +8,7 @@ const { UserModel } = require('../model/index');
 // Signup Route -- create a new user
 // Two ways to test this route with httpie
 // echo '{"username":"john","password":"foo"}' | http post :3000/signup
-// http post :3000/signup usernmae=john password=foo
+// http post :3000/signup username=john password=foo
 router.post('/signup', async (req, res) => {
 
   try {
