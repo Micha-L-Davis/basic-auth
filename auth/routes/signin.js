eedb118cfa9af2d4ef7dacd40f53e6f5eecae6c5
Micha Davis <m.leigh.davis@gmail.com>
2022-03-28 18:01:54 -0700

    add signin route module

---
+++ auth/routes/signin.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const { response } = require('express');
+const authenticate = require('../auth');
+
+
+// Signin Route -- login with username and password
+// test with httpie
+// http post :3000/signin -a john:foo
+router.post('/signin', authenticate, (req, res) => {
+  response.status(200).send(req.user);
+});
+
+module.exports = router;
